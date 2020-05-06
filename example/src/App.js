02beec2c94043057e111b0017fdb2e65b5b2fc1a
Makan <makan.edrisi@gmail.com>
2020-05-07 07:08:37 +0800

    start restructuring

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,86 @@
+/*
+ * Example to show  how auto-scroll-container
+ * component is working
+ *
+ * @link https://github.com/makannew/auto-scroll-container
+ * @file index.js
+ * @author Makan Edrisi
+ * @since 2020
+ *
+ */
 import React from 'react'
 
-import { ExampleComponent } from 'auto-scroll-container'
-import 'auto-scroll-container/dist/index.css'
+import { AutoScrollContainer } from 'auto-scroll-container'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <AutoScrollContainer
+      className='first-scroll'
+      contentClass='first-scroll-content'
+      topVoidRatio={1}
+      bottomVoidRatio={1}
+      iniPosY={0}
+      iniPosYOffset={0}
+    >
+      <h4 id='label1'>First Scroll</h4>
+      <h4 id='label2'>First scroll Content</h4>
+      <h4 id='label3'>Second Scroll</h4>
+
+      <AutoScrollContainer
+        className='second-scroll'
+        contentClass='second-scroll-content'
+      >
+        <h4 id='label4'>Second scroll Content</h4>
+        <div className='form-container'>
+          <form>
+            <h3>Sample content 1</h3>
+            <input type='text' placeholder='First Name' />
+            <input type='text' placeholder='Last Name' />
+            <div>
+              <div>
+                <input
+                  type='radio'
+                  id='option1'
+                  name='favorit'
+                  value='option1'
+                />
+                <label htmlFor='option1'>Option 1</label>
+              </div>
+              <div>
+                <input
+                  type='radio'
+                  id='option2'
+                  name='favorit'
+                  value='option2'
+                />
+                <label htmlFor='option2'>Option 2</label>
+              </div>
+              <div>
+                <input
+                  type='radio'
+                  id='option3'
+                  name='favorit'
+                  value='option3'
+                />
+                <label htmlFor='option3'>Option 3</label>
+              </div>
+            </div>
+            <input type='text' placeholder='Phone Number' />
+            <textarea placeholder='Address' rows='5' />
+            <input type='submit' name='Submit' />
+          </form>
+        </div>
+        <div className='form-container'>
+          <form>
+            <h3>Sample content 2</h3>
+            <input type='text' placeholder='First Name' />
+            <input type='text' placeholder='Last Name' />
+            <input type='submit' name='Submit' />
+          </form>
+        </div>
+      </AutoScrollContainer>
+    </AutoScrollContainer>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,189 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useRef, useLayoutEffect, useState, useEffect } from 'react'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const AutoScrollContainer = ({
+  children,
+  className = '',
+  contentClass = '',
+  injectMargin = { top: 0.9, bottom: 0.9, left: 0, right: 0 },
+  topVoidRatio = 0.9, // fraction of visible div height 0 to 1
+  bottomVoidRatio = 0.9, // fraction of visible div height 0 to 1
+  leftVoidRatio = 0,
+  rightVoidRatio = 0,
+  moveScroll = {
+    contentWidthRatio: 0,
+    viewWidthRatio: 0,
+    contentHeightRatio: 0,
+    viewHeightRatio: 0
+  },
+  iniPosX = 0,
+  iniPosXOffset = 0,
+  iniPosY = 0, // fraction of all content height 0 to 1
+  iniPosYOffset = 0.2, // fraction of visible div height 0 to 1
+  defaultViewPoint = { x: 0.5, y: 0.5 }
+}) => {
+  const scrollDiv = useRef()
+  const contentDiv = useRef()
+  const pos = useRef({ x: 0, offsetX: 0, y: 0, offsetY: 0 }).current
+  const voidSpace = useRef({ top: 0, bottom: 0, left: 0, right: 0 }).current
+  const currentFocus = useRef(null)
+  const [divSize, setDivSize] = useState(() => null)
+
+  const resetScrollPos = (width, height) => {
+    if (currentFocus.current) {
+      scrollDiv.current.scrollTop = pos.y
+      console.log(pos.y)
+    } else {
+      const scrollHeight = scrollDiv.current.scrollHeight
+      const scrollWidth = scrollDiv.current.scrollWidth
+      const [top, left] = getScrollPos({
+        ...pos,
+        divSize,
+        scrollWidth,
+        scrollHeight
+      })
+      scrollDiv.current.scrollLeft = left
+      scrollDiv.current.scrollTop = top
+    }
+  }
+
+  const setVoidSpace = (width, height) => {
+    const contentStyle = contentDiv.current.style
+    if (topVoidRatio) {
+      voidSpace.top = topVoidRatio * height
+      contentStyle.marginTop = `${voidSpace.top}px`
+    }
+    if (bottomVoidRatio) {
+      voidSpace.bottom = bottomVoidRatio * height
+      contentStyle.marginBottom = `${voidSpace.bottom}px`
+    }
+    if (leftVoidRatio) {
+      voidSpace.left = leftVoidRatio * width
+      contentStyle.marginLeft = `${voidSpace.left}px`
+    }
+    if (rightVoidRatio) {
+      voidSpace.right = rightVoidRatio * width
+      contentStyle.marginRight = `${voidSpace.right}px`
+    }
+  }
+
+  const getScrollPos = ({
+    x,
+    offsetX,
+    y,
+    offsetY,
+    divSize,
+    scrollWidth,
+    scrollHeight
+  }) => {
+    const topVoid = divSize.height * topVoidRatio
+    const bottomVoid = divSize.height * bottomVoidRatio
+    const contentHeight = scrollHeight - topVoid - bottomVoid
+    const leftVoid = divSize.width * leftVoidRatio
+    const rightVoid = divSize.width * rightVoidRatio
+    const contentWidth = scrollWidth - leftVoid - rightVoid
+    const top = y * contentHeight - offsetY * divSize.height + topVoid
+    const left = x * contentWidth - offsetX * divSize.width + leftVoid
+
+    return [top, left]
+  }
+
+  const setPosFromFocus = (x, y, focusX, focusY) => {}
+  const handleScroll = (e) => {
+    // pos.x =
+    //   (e.target.scrollLeft ? e.target.scrollLeft : 0) / e.target.scrollWidth
+    // pos.y =
+    //   (e.target.scrollTop ? e.target.scrollTop : 0) / e.target.scrollHeight
+    // console.log(scrollDiv.current.scrollHeight, contentDiv.current.scrollHeight)
+  }
+
+  const handleFocus = (e) => {
+    currentFocus.current = {
+      x: e.target.offsetLeft,
+      y: e.target.offsetTop
+    }
+    setPosFromFocus(currentFocus.current.x, currentFocus.current.y)
+  }
+
+  const handleBlure = (e) => {
+    currentFocus.current = null
+  }
+
+  useEffect(() => {
+    if (divSize === null) return
+    if (currentFocus.current) {
+      scrollDiv.current.scrollTop = pos.y
+      console.log(pos.y)
+    } else {
+      const scrollHeight = scrollDiv.current.scrollHeight
+      const scrollWidth = scrollDiv.current.scrollWidth
+      const [top, left] = getScrollPos({
+        ...pos,
+        divSize,
+        scrollWidth,
+        scrollHeight
+      })
+      scrollDiv.current.scrollLeft = left
+      scrollDiv.current.scrollTop = top
+    }
+  }, [divSize])
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      const divComputedStyle = getComputedStyle(scrollDiv.current)
+      const height = parseInt(divComputedStyle.height)
+      const width = parseInt(divComputedStyle.width)
+      setVoidSpace(width, height)
+      resetScrollPos(width, height)
+      setDivSize(() => ({ width, height }))
+    }
+    addEventListener('resize', handleResize)
+    handleResize()
+    return () => {
+      removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (divSize === null) return
+    setVoidSpace(divSize.width, divSize.height)
+  }, [bottomVoidRatio, topVoidRatio, leftVoidRatio, rightVoidRatio])
+
+  const [toggleUpdate, setToggleUpdate] = useState(false)
+  useEffect(() => {
+    if (divSize === null) {
+      setToggleUpdate(!toggleUpdate)
+      return
+    }
+    const scrollHeight = scrollDiv.current.scrollHeight
+    const scrollWidth = scrollDiv.current.scrollWidth
+    const [top, left] = getScrollPos({
+      x: iniPosX,
+      offsetX: iniPosXOffset,
+      y: iniPosY,
+      offsetY: iniPosYOffset,
+      divSize,
+      scrollWidth,
+      scrollHeight
+    })
+    scrollDiv.current.scrollLeft = left
+    scrollDiv.current.scrollTop = top
+    pos.x = iniPosX
+    pos.offsetX = iniPosXOffset
+    pos.y = iniPosY
+    pos.offsetY = iniPosYOffset
+  }, [iniPosX, iniPosXOffset, iniPosY, iniPosYOffset, toggleUpdate])
+
+  return (
+    <div
+      ref={scrollDiv}
+      className={className}
+      onScroll={handleScroll}
+      onFocusCapture={handleFocus}
+      onBlurCapture={handleBlure}
+    >
+      <div ref={contentDiv} className={contentClass}>
+        {children}
+      </div>
+    </div>
+  )
 }
